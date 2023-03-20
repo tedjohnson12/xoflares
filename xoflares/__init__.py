b5b97797c9079ef9f34b7905c004cbb03439d5f1
Ted Johnson <tedjohnson12@g.ucla.edu>
2023-03-20 17:06:58 -0400

    remove functions from init

--- xoflares/__init__.py
+++ xoflares/__init__.py
@@ -2,14 +2,7 @@
 
 from .version import __version__
 from .xoflares import (
-    multiflaremodel,
-    # multiflare,
     multiflaremodelnp,
     get_light_curvenp,
-    get_light_curve,
-    eval_get_light_curve,
-    multiflareintegral,
     multiflareintegralnp,
-    eval_multiflareintegral,
-    multiflare
 )
